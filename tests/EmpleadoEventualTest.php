538196bc1bb7c98f4ddf2ee8f1b48739079a1d95
Esteban <estebanvalladar@gmail.com>
2021-10-31 20:14:21 -0300

    Se agregan 2 tests, calculo de ingreso total, y el de prueba de monto negativo o cero

--- tests/EmpleadoEventualTest.php
+++ tests/EmpleadoEventualTest.php
@@ -10,6 +10,22 @@ class Empleado_eventual_test extends\PHPUnit\Framework\TestCase
 
 	$this->assertEquals((8500+13000)/2 *0.05,$Emp->calcularComision());
 	}
+
+	public function test_Calculo_el_Ingreso_total()
+    {
+	$Emp=new \App\EmpleadoEventual($nombre="Esteban",$apellido="Valladar",$dni=30484549,$salario=58000,$montosDeVentas=[8500, 13000]);
+
+	$this->assertEquals(($Emp->calcularComision()+$salario),$Emp->calcularIngresoTotal());
+
+	}
+	
+	public function test_Prueba_el_monto_negativo_o_cero()
+    {
+    $this->expectException(\Exception::class);
+
+    $Emp=new \App\EmpleadoEventual($nombre="Esteban",$apellido="Valladar",$dni=30484549,$salario=58000,$montosDeVentas=[8500,-200]);
+		
+    }
 	
 	
 }
